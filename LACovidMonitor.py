229c25283fa0427044678185030fce4d3d67d22a
Rafael Nuguid <raf.nuguid@gmail.com>
2020-05-03 21:40:46 -0700

    rearrange code

--- LACovidMonitor.py
+++ LACovidMonitor.py
@@ -140,101 +140,63 @@ def parseReports(reports):
         
     return parsed
 
-def run(saveFigs=False, useCached=True):
-    if useCached:
-        reports = getReportsCached()
-    else:
-        updateCache = True # Why not... should always update cache if we are checking the server!
-        
-        mainReportPg = getMainReportPage(updateCache)
-        reports = getReports(mainReportPg, updateCache)
-        
-    # Now get the data.
-    numberHospitalized = np.array([])
-    numberDied = np.ndarray([0])
-    dates = []
+"""
+Running average and median
+"""
+def simpWinFilt(arr, winSz=7):
+    winSzH = int(winSz/2)
+    y = np.ndarray(len(arr)-winSz + 1)
+    yM = np.ndarray(len(arr)-winSz + 1)
+    for i in np.arange(winSzH,len(arr)-winSzH):
+        y[i-winSzH] = np.sum(arr[i-winSzH:i+winSzH+1]) / winSz
+        yM[i-winSzH] = np.median(arr[i-winSzH:i+winSzH+1])
     
-    z = {'One':1,'Two':2,'Three':3,'Four':4,'Five':5,'Six':6,'Seven':7,'Eight':8,'Nine':9}
+    return y,yM
 
-    for report in reports:
-
-        mats = re.findall(r'Hospitalized \(Ever\)\\t(?P<numb>[0-9]+)', report)
-        if mats:
-            numberHospitalized = np.append(numberHospitalized, int(mats[0]))
-        else:
-            numberHospitalized = np.append(numberHospitalized, 0)
-            
-        # Oddly, this doesn't detect some of the earlier dates, need to investigaet.
-        mats = re.findall(r't(?P<z>[\w]+[\s][0-9]{2}, 2020)', report)
-        if mats:
-            dates += [mats[0]]
-        else:
-            dates += ['???',]
-        
-        mats = re.findall(r'([0-9]+|One|Two|Three|Four|Five|Six|Seven|Eight|Nine) New Death', report)
-        if mats:
-            try:
-                numberDied = np.append(numberDied, int(mats[0]))
-            except:
-                numberDied = np.append(numberDied, z[mats[0]])
-        else:
-            numberDied = np.append(numberDied, 0)
-        
-        # Hrm... lets look into this.
-        mats = re.findall(r'testing results [\w\s,%]+', report)
-        if mats:
-            print(mats)  
-        else:
-            print('No results.')
-        
+def makePlots(parsedReports, saveFigs=False):
+    
+    pR = parsedReports
+    latest = pR[-1]["date"]
+    # Total hospitalizations
     f0 = plt.figure()
-    plt.plot(numberHospitalized, 'r.')
-    plt.xlabel(f'Days since first hospitalization report (Last Upd {dates[-1]})')
+    numHospitalized = np.array([p['tHosp'] for p in pR])
+    plt.plot(numHospitalized, 'r.')
+    plt.xlabel(f'Days since first hospitalization report (Last Upd {latest})')
     plt.ylabel('# Ever Hospitalized (COVID-19)')
     plt.title('LA County (Hospitalizations)')
     plt.grid('on')
     plt.xlim(left=0)
     plt.ylim(bottom=0)
     
+    # New hospitalizations with 7-day running median
     f1 = plt.figure()
-    plt.plot(numberHospitalized[1:] - numberHospitalized[0:-1], 'r.')
-    plt.xlabel(f'Days since first hospitalization report (Last Upd {dates[-1]})')
+    newHospitalized = numHospitalized[1:] - numHospitalized[0:-1]
+    plt.plot(newHospitalized, 'r.')
+    plt.xlabel(f'Days since first hospitalization report (Last Upd {latest})')
     plt.ylabel('# New Hospitalizations (COVID-19)')
     plt.title('LA County (Hospitalizations)')
     plt.grid('on')
     plt.xlim(left=0)
     plt.ylim(bottom=0)
-    
-    dH = numberHospitalized[1:] - numberHospitalized[0:-1]
     winSz = 7
     winSzH = int(winSz/2)
-    y = np.ndarray(len(dH)-winSz + 1)
-    yM = np.ndarray(len(dH)-winSz + 1)
-    for i in np.arange(winSzH,len(dH)-winSzH):
-        y[i-winSzH] = np.sum(dH[i-winSzH:i+winSzH+1]) / winSz
-        yM[i-winSzH] = np.median(dH[i-winSzH:i+winSzH+1])
-    plt.plot(np.arange(winSzH,len(dH)-winSzH), y, 'b')
-    
+    [newHospMed, newHospAvg] = simpWinFilt(newHospitalized, winSz)
+    plt.plot(np.arange(winSzH,len(newHospitalized)-winSzH), newHospMed, 'b')
+
+    # New hospitalizations with 7-day running average    
     f2 = plt.figure()
-    plt.plot(numberHospitalized[1:] - numberHospitalized[0:-1], 'r.')
-    plt.xlabel(f'Days since first hospitalization report (Last Upd {dates[-1]})')
+    plt.plot(newHospitalized, 'r.')
+    plt.xlabel(f'Days since first hospitalization report (Last Upd {latest})')
     plt.ylabel('# New Hospitalizations (COVID-19)')
     plt.title('LA County (Hospitalizations)')
     plt.grid('on')
     plt.xlim(left=0)
     plt.ylim(bottom=0)
-    plt.plot(np.arange(winSzH,len(dH)-winSzH), yM, 'g')
+    plt.plot(np.arange(winSzH,len(newHospitalized)-winSzH), newHospAvg, 'b')
     
-    dH = numberDied
-    winSz = 7
-    winSzH = int(winSz/2)
-    y = np.ndarray(len(dH)-winSz + 1)
-    yM = np.ndarray(len(dH)-winSz + 1)
-    for i in np.arange(winSzH,len(dH)-winSzH):
-        y[i-winSzH] = np.sum(dH[i-winSzH:i+winSzH+1]) / winSz
-        yM[i-winSzH] = np.median(dH[i-winSzH:i+winSzH+1])
-    
+    # Number of people passed away (per day) w/ 7-day running average
     f3 = plt.figure()
+    numberDied = [p['dDeaths'] for p in pR]
     plt.plot(numberDied, 'r.')
     plt.xlabel(f'Days')
     plt.ylabel('# Deaths')
@@ -242,9 +204,10 @@ def run(saveFigs=False, useCached=True):
     plt.grid('on')
     plt.xlim(left=0)
     plt.ylim(bottom=0)
-    plt.plot(np.arange(winSzH,len(dH)-winSzH), y, 'b')
+    [numberDiedMed, numberDiedAvg] = simpWinFilt(numberDied, winSz)
+    plt.plot(np.arange(winSzH,len(numberDied)-winSzH), numberDiedAvg, 'b')
 
-
+    # Number of people passed away (per day) w/ 7-day running median
     f4 = plt.figure()
     plt.plot(numberDied, 'r.')
     plt.xlabel(f'Days')
@@ -253,10 +216,10 @@ def run(saveFigs=False, useCached=True):
     plt.grid('on')
     plt.xlim(left=0)
     plt.ylim(bottom=0)
-    plt.plot(np.arange(winSzH,len(dH)-winSzH), yM, 'b')
+    plt.plot(np.arange(winSzH,len(numberDied)-winSzH), numberDiedMed, 'b')
         
     if saveFigs:
-        newDir = dates[-1].replace(" ","_").replace(",","")
+        newDir = latest.replace(" ","_").replace(",","")
         if not os.path.exists(DEFAULT_FIG_DIR):
             os.mkdir(DEFAULT_FIG_DIR)
             
@@ -268,8 +231,24 @@ def run(saveFigs=False, useCached=True):
         f1.savefig(os.path.join(saveDir, 'f1.png'))
         f2.savefig(os.path.join(saveDir, 'f2.png'))
         f3.savefig(os.path.join(saveDir, 'f3.png'))
-        f4.savefig(os.path.join(saveDir, 'f4.png'))
+        f4.savefig(os.path.join(saveDir, 'f4.png'))    
 
+def run(saveFigs=False, useCached=True):
+    if useCached:
+        # Use local version of reports
+        reports = getReportsCached()
+    else:
+        # Pull data from LA County Public Health and update our cache
+        updateCache = True 
+        mainReportPg = getMainReportPage(updateCache)
+        reports = getReports(mainReportPg, updateCache)
+    
+    # Parse the daily reports
+    parsed = parseReports(reports)
+    
+    # Make our plots
+    makePlots(parsed, saveFigs=saveFigs)
+    
     return
 
 if __name__ == "__main__":
