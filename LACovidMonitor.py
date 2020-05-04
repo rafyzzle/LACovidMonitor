8f94c6306abf64014a6097fc6b2f2045ba8cd585
Rafael Nuguid <raf.nuguid@gmail.com>
2020-05-03 18:47:52 -0700

    update comment header

--- LACovidMonitor.py
+++ LACovidMonitor.py
@@ -20,8 +20,7 @@ import re
 Simple independent tool to visualize COVID hospitalization, tests and death rates per
 LA County Public Health reports.
 
-Yes, as of May 3, they've got a fancy Surveillance Dashboard the public can use, but it 
-doesnt really have the metrics I'm interested in...
+To include some metrics not immediately available on http://dashboard.publichealth.lacounty.gov/covid19_surveillance_dashboard/
   --> # of NEW Daily Hospitalizations [DONE]
   --> # of Hospitalizations (Ever)    [DONE]
   --> # Currently Hospitalized <--- still looking for a good source for this #'
